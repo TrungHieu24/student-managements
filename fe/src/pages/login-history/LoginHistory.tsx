c9d9ad00d8cea921ade78b665cdce4b7ead0f169
hieutm47 <hieutm47@fpt.com>
2025-05-22 16:39:19 +0700

    Add more text.

--- fe/src/pages/login-history/LoginHistory.tsx
+++ fe/src/pages/login-history/LoginHistory.tsx
@@ -57,7 +57,7 @@ const LoginHistory = () => {
   return (
     <Paper sx={{ p: 3 }}>
       <Typography variant="h6" gutterBottom>
-        Lịch sử đăng nhập
+        Lịch sử đăng nhập của user.
       </Typography>
 
       {loading ? (
